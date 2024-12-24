884fe26760a12d1b35b4ebf3389c7dfe86f6b2e2
Rafael Secco <raf.secco@gmail.com>
2024-12-23 23:05:22 -0300

    update changes

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,7 @@ import database from '../../../../infra/database.js';
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result);
+  console.log(result.rows);
   response.status(200).json({ chave: "são acima da média" });
 }
 export default status;
