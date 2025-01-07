a488e364bdd1e3f21a940d8f285125a56439dea7
Rafael Secco <raf.secco@gmail.com>
2025-01-07 13:14:23 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

---
+++ infra/errors.js
@@ -0,0 +1,28 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno não esperado aconteceu.", {
+      cause: cause,
+    });
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte";
+    this.statusCode = 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+
+  // toJSON() {
+  //   return {
+  //     error: {
+  //       message: this.message,
+  //       cause: this.cause,
+  //     },
+  //   };
+  // }
+}
